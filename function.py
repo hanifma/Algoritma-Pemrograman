9d53223710837b6dacc8665d66ecab28da85214a
Hanif Muflihul <30397639+hanifma@users.noreply.github.com>
2020-02-11 11:32:54 +0700

    Create function.py
    Contoh progrum fungsi,input,variabel, dan penjumlahan

---
+++ function.py
@@ -0,0 +1,10 @@
+a = int(input("Masukan angka-1: "))
+b = int(input("Masukan angka-2: "))
+c = int(input("Masukan angka-3: "))
+
+def jumlah3():
+    x = a + b +c
+    print("Jumlah Angka Tersebut = ",x)
+    return
+
+jumlah3()
