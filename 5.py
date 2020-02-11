6bc13c3bc64f44852fc222d066a3d2e2d6b9f4af
Hanif Muflihul <30397639+hanifma@users.noreply.github.com>
2020-02-11 11:29:05 +0700

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,8 @@
+for a in range(5):
+    for b in range(a+1):
+        print('* ', end='')
+    print()
+for i in reversed(range(4)):
+    for j in range(i+1):
+        print('* ', end='')
+    print() 
